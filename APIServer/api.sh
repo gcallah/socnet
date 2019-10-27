9b2e5c66e7c4961a9473266859d719c87b003287
fh914 <fh914@nyu.edu>
2019-10-26 19:10:04 -0700

    Changing description in file to match SOCNET

--- APIServer/api.sh
+++ APIServer/api.sh
@@ -1,5 +1,5 @@
-# This shell script runs the Indra API server.
+# This shell script runs the SOCNET API server.
 # The user_type env var is needed to make user interactions within
-# Indra behave properly.
+# SOCNET behave properly.
 export user_type="api"
 python3 api_endpoints.py
