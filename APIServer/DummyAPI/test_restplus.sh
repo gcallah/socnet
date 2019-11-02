3ab8f4330ab16248e4bb43ff93ab2360fc944923
wm1065 <wm1065@nyu.edu>
2019-11-01 21:07:30 -0400

    Added a test script test_restplus.sh for api_restplus.py

---
+++ APIServer/DummyAPI/test_restplus.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+trap "exit" INT TERM ERR
+trap "kill 0" EXIT
+python api_restplus.py &
+sleep 5
+
+URL=http://127.0.0.1:5000
+
+KEY=$RANDOM
+RECORD="'{\"Date\" : \"201801129\", \"Time\" : \"12:32\",\"Type\" : \"C\", \"Location\" : \"NYC\", \"Text\" : \"Hello ${KEY}!\",\"Who\" : \"xyz\",\"Org\" : \"NYU\"}'"
+PutCommand="curl -X PUT ${URL}/alert/$KEY/ -H 'Content-Type: application/json' -d ${RECORD}"
+echo Test PUT API
+echo curl command is: $PutCommand
+eval $PutCommand
+echo
+echo
+
+echo Test GET API
+GetCommand="curl -X GET ${URL}/alert/$KEY/"
+echo curl command is: $GetCommand
+eval $GetCommand
+echo
+
