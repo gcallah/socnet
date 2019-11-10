4181edc809d2c84633080b494de9c313b7ba504d
gcallah <gcallah@mac.com>
2019-11-10 13:28:51 -0500

    Took utils out of pa_reload_webapp path.

--- rebuild.sh
+++ rebuild.sh
@@ -11,6 +11,6 @@ source /home/$project_name/.virtualenvs/$project_name/bin/activate
 # install all of our packages:
 pip install -r docker/requirements.txt
 echo "Going to reboot the webserver"
-API_TOKEN=$1 utils/pa_reload_webapp.py $project_name.pythonanywhere.com
+API_TOKEN=$1 pa_reload_webapp.py $project_name.pythonanywhere.com
 # the next line gives us evidence as to whether script ran on PA:
 touch reboot
