81e44e12207c59df9ab481c0e8420732d0ca375b
wm1065 <wm1065@nyu.edu>
2019-11-15 22:04:09 -0500

    Backing up socnet_pythonanywhere_com_wsgi.py for pythonanywhere

--- APIServer/socnet_pythonanywhere_com_wsgi.py
+++ APIServer/socnet_pythonanywhere_com_wsgi.py
@@ -1,7 +1,7 @@
 import sys
 
 # add your project directory to the sys.path
-project_home = u'/home/socnet/socnet/APIServer'
+project_home = u'/home/socnet/socnet'
 if project_home not in sys.path:
     sys.path = [project_home] + sys.path
 
