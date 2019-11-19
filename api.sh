f617de49b726a833cddf327474279ab9b7bb372d
wm1065 <wm1065@nyu.edu>
2019-11-18 23:39:38 -0500

    Moved APIServer/api.sh to the main directory in order to be consistent with our pythonanywhere server

--- api.sh
+++ api.sh
@@ -2,6 +2,5 @@
 # The user_type env var is needed to make user interactions within
 # SOCNET behave properly.
 export user_type="api"
-#export PYTHONPATH="$PWD"
-export PYTHONPATH="$PWD:${PWD%/*}"
+export PYTHONPATH="$PWD"
 python3 APIServer/api_endpoints.py
