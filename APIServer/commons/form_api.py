f0021c96d535831d3f9ad53056332ce662739ccf
fh914 <fh914@nyu.edu>
2019-11-30 11:12:56 -0800

    Starting function in validate_alert to check types against given entries

--- APIServer/commons/form_api.py
+++ APIServer/commons/form_api.py
@@ -1,4 +1,5 @@
 from APIServer.commons.api_utils import read_json
+import datetime
 
 
 def get_alert_form(path):
@@ -29,20 +30,33 @@ def validate_alert(alert_format, alert):
     Validate that the alert has the correct format
     """
 
-    required = alert_format['required']
-    for r in required:
-        test = alert.get(r, None)
-        if test is None:
-            return False, 'Missing {}'.format(r)
-
-        req_type = alert_format['properties'][r]['type']
-        given_type = type(test)
-        if req_type == 'string' and given_type != str:
-            return False, '{} not given as string'.format(r)
-        elif req_type == 'int' and given_type != int:
-            return False, '{} not given as int'.format(r)
+    def test_prop(prop, submitted):
+        type_dict = {}
+        req_type = prop.get('type', None)
+        if req_type is not None:
+            if submitted is not None:
+                pass
+            else:
+                return False, 'Missing entry of type {}'.format(req_type)
         else:
-            pass
-            # Object Type
+            return True, 'Nothing required'
+            
+
+    required = alert_format.get('required', None)
+    if required is not None:
+        for r in required:
+            test = alert.get(r, None)
+            if test is None:
+                return False, 'Missing {}'.format(r)
+
+            req_type = alert_format['properties'][r]['type']
+            given_type = type(test)
+            if req_type == 'string' and given_type != str:
+                return False, '{} not given as string'.format(r)
+            elif req_type == 'int' and given_type != int:
+                return False, '{} not given as int'.format(r)
+            else:
+                pass
+                # Object Type
 
     return True, ''
