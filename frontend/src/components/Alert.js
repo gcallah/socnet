6c19f7f1f9bcfe35430794561137979a5831b1be
Sabrena Beck <sb5829@nyu.edu>
2019-12-03 13:28:10 -0500

    started put alert page prototype

---
+++ frontend/src/components/Alert.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+
+class Alert extends Component {
+  constructor(){
+    super();
+    this.state = {
+      eventDetails: {},
+    }
+  }
+  render() {
+    return (
+      <div>
+      work in progress
+      </div>
+    );
+  }
+}
+
+export default Alert;
