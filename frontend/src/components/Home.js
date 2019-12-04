d73c307641c5a3bf31ee3b991c9085b6522961b2
Philip Jung <philip.jung.98@gmail.com>
2019-12-04 10:01:18 -0500

    added in get request for alerts in home component

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,19 +1,51 @@
 import React, { Component } from 'react';
-import { Loader, Dimmer } from 'semantic-ui-react';
+import axios from 'axios';
+import { Segment, Loader, Dimmer } from 'semantic-ui-react';
 import Header from './Header';
 
 class Home extends Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      loadingData: false,
+      alerts: [],
     };
+    this.apiServer = 'http://socnet.pythonanywhere.com/'
+  }
+
+  async componentDidMount() {
+    try {
+      this.setState({ loadingData: true });
+      const res = await axios.get(`${this.apiServer}alerts`);
+      this.setState({
+        alerts: res.data,
+        loadingData: false,
+      })
+      console.log(this.state);
+    } catch (e) {
+      console.log('error')
+    }
   }
 
   render() {
+    const { loadingData, alerts } = this.state
+
+    if (loadingData) {
+      return (
+        <Segment>
+          <Dimmer active inverted>
+            <Loader inverted>Loading</Loader>
+          </Dimmer>
+        </Segment>
+      );
+    }
+
     return (
       <div className="container">
         <Header title="Socnet" />
+        {alerts.map((alert) =>{
+          return alert
+        })}
       </div>
     );
   }
