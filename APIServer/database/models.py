7540b50791c705c7132b0eb14bc477fb8c77bd49
karan katliwala <krk386@nyu.edu>
2019-12-06 13:36:40 -0500

    added models.py to create alerts table.

---
+++ APIServer/database/models.py
@@ -0,0 +1,15 @@
+from APIServer import db
+ 
+class Alert(db.Model):
+    __tablename__ = "alerts"
+ 
+    id = db.Column(db.Integer, primary_key=True)
+    event_datetime = db.Column(db.DateTime)
+    event_zipcode = db.Column(db.Text)
+    event_city = db.Column(db.Text)
+    event_state = db.Column(db.Text)
+    event_country = db.Column(db.Text)
+    event_type = db.Column(db.Text)
+    event_description = db.Column(db.Text)
+    event_severity = db.Column(db.Text)
+    msg_sender = db.Column(db.Text)
