6554e65a09aecad7a558eb7901c044f3cc7998a3
Thomas Wang <mw3482@nyu.edu>
2020-02-29 00:22:48 -0500

    add new models

--- APIServer/database/models.py
+++ APIServer/database/models.py
@@ -13,3 +13,17 @@ class Alert(db.Model):
     event_description = db.Column(db.Text)
     event_severity = db.Column(db.Text)
     msg_sender = db.Column(db.Text)
+
+class Thread(db.Model):
+    __tablename__ = "threads"
+ 
+    id = db.Column(db.Integer, primary_key=True)
+    first_comment_id = db.Column(db.Integer)
+    last_comment_id = db.Column(db.Integer)
+
+class Comment(db.Model):
+    __tablename__ = "comments"
+ 
+    id = db.Column(db.Integer, primary_key=True,autoincrement=True)
+    content = db.Column(db.Text)
+    next_comment_id = db.Column(db.Integer)
