876ce774359987f7126fdf626343477a0b4d6380
wm1065 <wm1065@nyu.edu>
2020-03-09 22:00:35 -0400

    Modified format.py to fix the error that the message template file can not be found

--- APIServer/slack/format.py
+++ APIServer/slack/format.py
@@ -5,6 +5,8 @@ from APIServer.commons.api_utils import read_json
 def slack_format_alert(raw_alert):
 	MESSAGE_TEMPLATE = 'message.json'
 	message = read_json(MESSAGE_TEMPLATE)
+	if message.get('Error:', None):
+		message = read_json('APIServer/slack' + MESSAGE_TEMPLATE)
 
 	alert_id = raw_alert[0][0]
 	datetime = raw_alert[0][1]
@@ -15,7 +17,7 @@ def slack_format_alert(raw_alert):
 	sender = raw_alert[0][9]
 	url = 'https://gcallah.github.io/socnet/webapp.html#/thread/' + str(alert_id)
 
-	#message['blocks'][1]['text']['text'] = '*' + event + '*\n' + description
-	#message['blocks'][2]['elements']['text'] = location + '\n' + datetime + '\n' + severity + '\nby *' + sender + '*'
-	#message['blocks'][4]['accessory']['url'] = url
+	message['blocks'][1]['text']['text'] = '*' + event + '*\n' + description
+	message['blocks'][2]['elements']['text'] = location + '\n' + datetime + '\n' + severity + '\nby *' + sender + '*'
+	message['blocks'][4]['accessory']['url'] = url
 	return message
