2c37f015bb6847c40d04eab62ea60ca1e8adac8b
wm1065 <wm1065@nyu.edu>
2020-03-10 21:14:29 -0400

    Modified API token in rebuild.sh to fix auto-reload issue

--- rebuild.sh
+++ rebuild.sh
@@ -10,14 +10,7 @@ git pull origin master
 source /home/$project_name/.virtualenvs/$project_name/bin/activate
 # install all of our packages:
 pip install -r requirements/requirements.txt
-# check and install if pa_reload_webapp.py does not exist
-FILE=pa_reload_webapp.py
-if [ -f "$FILE" ]; then
-    echo "$FILE exist"
-else 
-    echo "$FILE does not exist"
-    pip3.5 install --user pythonanywhere
-fi
-API_TOKEN=$1 pa_reload_webapp.py $project_name.pythonanywhere.com
+echo "Going to reboot the webserver"
+API_TOKEN=849b1677af163ab7d893576b3f01aca7a13ccd22 pa_reload_webapp.py $project_name.pythonanywhere.com
 # the next line gives us evidence as to whether script ran on PA:
 touch reboot
