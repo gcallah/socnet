dd1b132e4881b309dcee493c59e50bfe0bc8bad6
wm1065 <wm1065@nyu.edu>
2020-03-10 20:59:07 -0400

    Modified rebuild.sh to make API server auto-reload after building

--- rebuild.sh
+++ rebuild.sh
@@ -10,7 +10,14 @@ git pull origin master
 source /home/$project_name/.virtualenvs/$project_name/bin/activate
 # install all of our packages:
 pip install -r requirements/requirements.txt
-echo "Going to reboot the webserver"
+# check and install if pa_reload_webapp.py does not exist
+FILE=pa_reload_webapp.py
+if [ -f "$FILE" ]; then
+    echo "$FILE exist"
+else 
+    echo "$FILE does not exist"
+    pip3.5 install --user pythonanywhere
+fi
 API_TOKEN=$1 pa_reload_webapp.py $project_name.pythonanywhere.com
 # the next line gives us evidence as to whether script ran on PA:
 touch reboot
