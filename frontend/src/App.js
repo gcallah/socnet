9f8a759f4319c712fdc9616a045f7f5836ad4d04
harmanchawla <hsc367@nyu.edu>
2020-03-14 03:48:45 -0400

    Tested version with new build.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,10 +4,9 @@ import 'semantic-ui-css/semantic.min.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Route, Switch, BrowserRouter } from 'react-router-dom';
 import Home from './components/Home';
-// import CreateAlert from './components/CreateAlert_ Deprecated';
 import ThreadView from './components/ThreadView';
 import FilterForm from './components/FilterForm';
-import CreartAlert from './components/NewCreateAlerts'
+import CreartAlert from './components/CreateAlerts'
 
 function App() {
   return (
