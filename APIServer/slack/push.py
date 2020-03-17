d5d4b4a74f5604b070183465a43c58b0d85e5388
wm1065 <wm1065@nyu.edu>
2020-03-17 18:49:02 -0400

    Modified push_to_channel function to open a modal(dialog) in Slack

--- APIServer/slack/push.py
+++ APIServer/slack/push.py
@@ -10,11 +10,11 @@ def push_to_slack(text):
 
 
 def push_to_channel(channel, trigger_id):
-	URL = 'https://slack.com/api/chat.postMessage'
+	URL = 'https://slack.com/api/views.open'
 	with open('APIServer/slack/dialog.json', 'r') as json_file:
 		data=json_file.read()
 	dialog = json.loads(data)
-	textToSend = {'channel': channel, 'text': dialog}
+	textToSend = {'channel': channel, 'trigger_id': trigger_id, 'view': dialog}
 	headers = {"Authorization": "Bearer xoxp-765850805047-790629649378-989309639635-00bf54bba4048c8414ad0983f630c586"}
 	response = requests.post(URL, json=textToSend, headers=headers)
 	return { response.status_code : response.text }
