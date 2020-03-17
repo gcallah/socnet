2a90c647f07b0130b3d5438d23ac551c0fb17830
wm1065 <wm1065@nyu.edu>
2020-03-17 18:33:56 -0400

    Modified push_to_channel method to echo correct info to the channel

--- APIServer/slack/push.py
+++ APIServer/slack/push.py
@@ -16,5 +16,5 @@ def push_to_channel(channel, trigger_id):
 	dialog = json.loads(data)
 	textToSend = {'channel': channel, 'text': dialog}
 	headers = {"Authorization": "Bearer xoxp-765850805047-790629649378-989309639635-00bf54bba4048c8414ad0983f630c586"}
-	response = requests.post(URL, data=textToSend, headers=headers)
+	response = requests.post(URL, json=textToSend, headers=headers)
 	return { response.status_code : response.text }
