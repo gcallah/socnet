1f39a416ce8e3c2f287ab9649b9f06f9eb673241
Thomas Wang <mw3482@nyu.edu>
2020-04-08 07:49:49 +0800

    initialize database migration

--- APIServer/__init__.py
+++ APIServer/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 from flask_marshmallow import Marshmallow
+from flask_migrate import Migrate
 
 db = SQLAlchemy()
 ma = Marshmallow()
@@ -12,6 +13,7 @@ def create_app(config):
     app.config['SQLALCHEMY_DATABASE_URI'] = config['sqlalchemy_database_uri']
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
+    Migrate(app, db)
     ma.init_app(app)
 
     with app.app_context():
