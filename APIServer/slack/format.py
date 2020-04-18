b82025fb8a5d3f1c623733fe2b04be17f2f86785
wm1065 <wm1065@nyu.edu>
2020-04-17 21:07:35 -0400

    Modfied filter alert template to add page information in the returned alerts

--- APIServer/slack/format.py
+++ APIServer/slack/format.py
@@ -106,7 +106,11 @@ def get_filter_params_from_slack(payload):
                     params[key[6:]] = values[value][key]['selected_date']
             else:
                 if values[value][key].get('value'):
-                    params[key[6:]] = values[value][key]['value']
+                    if key == 'page':
+                        page = int(values[value][key]['value'])
+                        params['offset'] = 0 * (page - 1)
+                    else:
+                        params[key[6:]] = values[value][key]['value']
     return params
 
 
