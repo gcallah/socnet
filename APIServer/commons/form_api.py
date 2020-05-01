56e04d4866ac0ae9a7272fa3b2195a213b2b9a07
wm1065 <wm1065@nyu.edu>
2020-05-01 18:49:49 -0400

    Removed 'event_' in form_api.py

--- APIServer/commons/form_api.py
+++ APIServer/commons/form_api.py
@@ -10,13 +10,13 @@ def create_alert_json(alert_tuple):
     Create alert_json from an alert tuple
     '''
     alert_json = {}
-    alert_json['event_datetime'] = alert_tuple[1]
-    alert_json['event_zipcode'] = alert_tuple[2]
-    alert_json['event_city'] = alert_tuple[3]
-    alert_json['event_state'] = alert_tuple[4]
-    alert_json['event_country'] = alert_tuple[5]
-    alert_json['event_type'] = alert_tuple[6]
-    alert_json['event_description'] = alert_tuple[7]
-    alert_json['event_severity'] = alert_tuple[8]
-    alert_json['msg_sender'] = alert_tuple[9]
+    alert_json['datetime'] = alert_tuple[1]
+    alert_json['zipcode'] = alert_tuple[2]
+    alert_json['city'] = alert_tuple[3]
+    alert_json['state'] = alert_tuple[4]
+    alert_json['country'] = alert_tuple[5]
+    alert_json['type'] = alert_tuple[6]
+    alert_json['description'] = alert_tuple[7]
+    alert_json['severity'] = alert_tuple[8]
+    alert_json['sender'] = alert_tuple[9]
     return alert_json
