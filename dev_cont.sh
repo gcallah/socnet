8611da06d110041e8cfa8a5db1aa94dd3987f476
wm1065 <wm1065@nyu.edu>
2020-05-09 00:06:36 -0400

    Removed the command that removes lingering containers since the --rm option makes an automatically clean up upon exit

--- dev_cont.sh
+++ dev_cont.sh
@@ -6,7 +6,5 @@ then
     HOST_PORT=$1
 fi
 
-echo "Going to remove any lingering $REPO-dev container."
-docker rm $REPO-dev 2> /dev/null || true
 echo "Now running docker to spin up the container."
 docker run -it -p $HOST_PORT:8000 -v $PWD:/home/$REPO --rm gcallah/$REPO-dev bash
