2df283f56df28df7d59dee2935b2eb404d142104
gcallah <gcallah@mac.com>
2020-06-16 17:28:48 -0400

    Updates

---
+++ precache-manifest.157a95febcac70a5b3d31a2a126e757b.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24858113ee6057b3eec59228def0d378",
+    "url": "/socnet/index.html"
+  },
+  {
+    "revision": "d63d62a4aea8d4ef85c3",
+    "url": "/socnet/static/css/2.65740463.chunk.css"
+  },
+  {
+    "revision": "9efd0aea7c639e7d4d6c",
+    "url": "/socnet/static/css/main.8a6b00d7.chunk.css"
+  },
+  {
+    "revision": "d63d62a4aea8d4ef85c3",
+    "url": "/socnet/static/js/2.407d113f.chunk.js"
+  },
+  {
+    "revision": "280640add9ea6f0757e21216374de3d5",
+    "url": "/socnet/static/js/2.407d113f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9efd0aea7c639e7d4d6c",
+    "url": "/socnet/static/js/main.f257ed24.chunk.js"
+  },
+  {
+    "revision": "36bf37e1790cebed887b",
+    "url": "/socnet/static/js/runtime-main.1a376210.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/socnet/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/socnet/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/socnet/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/socnet/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/socnet/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/socnet/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/socnet/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/socnet/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/socnet/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/socnet/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/socnet/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/socnet/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/socnet/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/socnet/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/socnet/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/socnet/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
